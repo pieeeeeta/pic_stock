95c8e224c3e645b2d4cfd1ecdf2e677548f5c2c4
Takuho Tada <pieeeeeta@gmail.com>
2019-03-02 19:46:49 +0900

    form_color

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "tada.takuho@gmail.com", #gmailアドレス
+    :password => "3701takuho", #gmailパスワード
+    :authentication => 'login',
+  }
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
