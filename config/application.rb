cba048851d8a15a2bc93361374826b2e33fcf092
Takuho Tada <pieeeeeta@gmail.com>
2019-03-03 14:44:01 +0900

    edit application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-    #herokuからのメール送信のための設定?
-    config.assets.initialize_on_precompile = false
+
 
 
 module PicStock
@@ -15,6 +14,8 @@ module PicStock
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+        #herokuからのメール送信のための設定?
+        config.assets.initialize_on_precompile = false
 
 
     # Settings in config/environments/* take precedence over those specified here.
