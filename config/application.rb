d3226fc7b7d7e783a5b57ba8f5f0569c59a2361e
Takuho Tada <pieeeeeta@gmail.com>
2019-03-03 14:39:31 +0900

    edit application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module PicStock
     config.load_defaults 5.2
 
     #herokuからのメール送信のための設定?
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
