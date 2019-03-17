3bf2d3bb118474be77f9826e68421253cc47d798
Takuho Tada <pieeeeeta@gmail.com>
2019-03-17 12:56:01 +0900

    add show page

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -1,6 +1,6 @@
 class ToppageController < ApplicationController
   def index
-    @picposts = Picpost.order('id DESC').limit(9)
+    @picposts = Picpost.order('id DESC').limit(12)
 
     @picposts_ranking = Picpost.order('likes_count DESC').limit(9)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @like_picposts = @user.like_picposts
   end
 end
