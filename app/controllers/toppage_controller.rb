28a8208b18a743fb3fdc1b0c7dfe03d197b6d880
Takuho Tada <pieeeeeta@gmail.com>
2019-03-17 12:19:33 +0900

    add popularposts

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -1,5 +1,7 @@
 class ToppageController < ApplicationController
   def index
-    @picposts = Picpost.order('id DESC').limit(15)
+    @picposts = Picpost.order('id DESC').limit(9)
+
+    @picposts_ranking = Picpost.order('likes_count DESC').limit(9)
   end
 end
